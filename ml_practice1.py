d800b90bda05dee6c763634d313984894fd079f4
Danyal Abbas <abasdaniel61@gmail.com>
2025-01-08 11:26:23 +0500

    Update ml_practice1.py

--- ml_practice1.py
+++ ml_practice1.py
@@ -5,7 +5,7 @@ from sklearn.metrics import mean_squared_error
 
 
 diabetes = datasets.load_diabetes()
-# dict_keys(['data', 'target', 'frame', 'DESCR', 'feature_names', 'data_filename', 'target_filename', 'data_module'])
+## dict_keys(['data', 'target', 'frame', 'DESCR', 'feature_names', 'data_filename', 'target_filename', 'data_module'])
 
 diabetes_X = diabetes.data[:,np.newaxis, 2]
 # print(diabetes_X)
